3960c5abb2bdc9a7a45c1d3ba8281e7a5fac06c2
Dmitry Shtakin <dkshtakin@edu.hse.ru>
2021-12-09 00:12:34 +0300

    Fixes in GetImports.py

--- ImportsAndNames/GetImports.py
+++ ImportsAndNames/GetImports.py
@@ -1,13 +1,5 @@
+from . import Utils
 import os
-
-
-def remove_file(path: str):
-    """
-    must be used for removing path.txt and wabbit_sequences_imports_0.txt
-    :param path: path of file which should be removed
-    """
-    if os.path.exists(path):
-        os.remove(path)
 
 
 def get_imports(path: str):
@@ -20,9 +12,9 @@ def get_imports(path: str):
     cmd = "cd ImportsAndNames/buckwheat && python3 -m buckwheat.run --local -i ../path.txt -o ../ -g imports"
     os.system(cmd)
     imports = set()
-    remove_file("ImportsAndNames/path.txt")
+    Utils.remove_file("ImportsAndNames/path.txt")
     with open('ImportsAndNames/wabbit_sequences_imports_0.txt', 'r', encoding='utf-8') as r:
         for line in r:
             imports.add(line.strip())
-    remove_file("ImportsAndNames/wabbit_sequences_imports_0.txt")
+    Utils.remove_file("ImportsAndNames/wabbit_sequences_imports_0.txt")
     return imports
